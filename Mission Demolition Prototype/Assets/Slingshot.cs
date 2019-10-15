a6c877667cf6d4180dd0535942d53e5486ca94ff
chrisg8299 <cgrimes@student.sussex.edu>
2019-10-15 15:34:54 -0400

    mission

---
+++ Mission Demolition Prototype/Assets/Slingshot.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float velocityMult = 8f;
+
+
+
+
+    [Header("Set Dynamically")]
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    private Rigidbody projectileRigidbody;
+
+    public GameObject prefabProjectile;
+
+
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint"); //
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+
+
+    }
+    void OnMouseEnter()
+    {
+        print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+    void OnMouseDown()
+
+    {
+
+
+
+        aimingMode = true;
+
+        projectile = Instantiate(prefabProjectile) as GameObject;
+
+        projectile.transform.position = launchPos;
+
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
+
